3b59f4f5ef36cb7007aa3701d4ca3bc9f9e40da8
Nathan Custodio Rosa <82404529+nathan79br@users.noreply.github.com>
2023-12-06 15:15:38 -0300

    Update MainActivity.java
    adicionando função de reconhecimento de fala e transcrição para a caixa de texto

--- app/src/main/java/com/fatec/speechrecognition/MainActivity.java
+++ app/src/main/java/com/fatec/speechrecognition/MainActivity.java
@@ -1,20 +1,24 @@
 package com.fatec.speechrecognition;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView microphoneImageView;
     private boolean isMicrophoneOn = false;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        text = findViewById(R.id.text);
         microphoneImageView = findViewById(R.id.mic);
 
         // Adicione um listener de clique ao ImageView
@@ -27,10 +31,26 @@ public class MainActivity extends AppCompatActivity {
                 // Change the image based on the microphone state
                 if (isMicrophoneOn) {
                     microphoneImageView.setImageResource(R.drawable.opened_mic);
+                    speak(microphoneImageView);
                 } else {
                     microphoneImageView.setImageResource(R.drawable.block_mic);
                 }
             }
         });
+    }
+
+    public void speak(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Começe a falar");
+        startActivityForResult(intent, 100);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            text.setText(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0));
+        }
     }
 }
